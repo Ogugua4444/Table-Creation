2aba4fd1c4c1972e5599830bd2216e0b6ac2df1b
Ogugua Onyeka <122956461+Ogugua4444@users.noreply.github.com>
2023-12-09 03:00:28 +0100

    Add files via upload

---
+++ Creating Table in SQL.sql
@@ -0,0 +1,27 @@
+Create table Employeesalary 
+(EmployeeID int, 
+JobTitle varchar(50),
+Salary int)
+
+
+Insert into Employeesalary
+Values (1001, 'Manager', '100000'),
+(1002, 'Saleman', '52000'),
+(1003, 'HR', '65000'),
+(1004, 'RegionalHead', '70000'),
+(1005, 'Operations', '45000'),
+(1006, 'HR', '60000'),
+(1007, 'Analyst', '55000'),
+(1008, 'CustomerCare', '50000'),
+(1009, 'Salesman', '52000'),
+(1010, 'Auditor', '62000'),
+(1011, 'Accountant', '65000'),
+(1012, 'Admin', null),
+(1013, null, 20000),
+(1014, 'Cleaner', 5000), 
+(1015, 'Security', 30000),
+(1016,null, null),
+(null, null, null),
+(1018, 'Cashier', 7000),
+(1019, 'Logistics', 15000),
+(1020, null, 20000)
